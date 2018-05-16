bb56705238b560d56fb533376995564ad2ff2978
elipinska <ewa.lipinska@hotmail.com>
2018-05-16 16:41:37 +0100

    Pass all the tests

---
+++ models/traveller.js
@@ -0,0 +1,50 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map(journey => journey.startLocation);
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map(journey => journey.endLocation);
+};
+
+Traveller.prototype.getModesOfTransport = function () {
+  return this.journeys.map(journey => journey.transport);
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter(journey => journey.transport === transport);
+
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter(journey => journey.distance >= minDistance);
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+    return total + journey.distance;
+  }, 0)
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function() {
+  const uniqueModesOfTransport = [];
+
+  this.journeys.forEach(function(journey) {
+      if (!uniqueModesOfTransport.includes(journey.transport)) {
+        uniqueModesOfTransport.push(journey.transport);
+      };
+    });
+
+  return uniqueModesOfTransport;
+
+
+  // const allModesOfTransport = this.getModesOfTransport();
+  //
+  // return allModesOfTransport.filter((mode, position) => { return allModesOfTransport.indexOf(mode) === position});
+};
+
+
+module.exports = Traveller;
